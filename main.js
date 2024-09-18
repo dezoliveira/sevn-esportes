abf97a9a56e6d3c56b0602e582029746c39a6507
Andres Oliveira <andresoliveira@protonmail.com>
2024-09-18 00:05:31 -0300

    add: roundCard

--- main.js
+++ main.js
@@ -1,10 +1,32 @@
-import { roundCard } from './src/scripts/roundCard'
+import { renderCard } from './src/scripts/roundCard'
 import { getRound } from './src/services/sevnApi'
+
+let counter = 1
 
 window.addEventListener("load", async () => {
   const data = await getRound()
   
   if (data !== undefined) {
-    roundCard(data)
+    renderCard(data, counter++)
+
+    const roundBack = document.getElementById("roundBack")
+
+    roundBack.addEventListener("click", () => {
+      console.log('roundBack', counter)
+
+      if (counter > 0) {
+        renderCard(data, counter--)
+      }
+    })
+
+    const roundNext = document.getElementById("roundNext")
+
+    roundNext.addEventListener("click", () => {
+      console.log('roundNext', counter)
+
+      if (counter < data.length) {
+        renderCard(data, counter++)
+      }
+    })
   }
 })
--- src/scripts/roundCard.js
+++ src/scripts/roundCard.js
@@ -1,8 +1,12 @@
-export const roundCard = (rounds, counter=1) => {
+export const renderCard = (rounds, counter) => {
+
+  let activeRound = counter
+
+  console.log('round', counter)
 
   const roundNumber = document.getElementById("roundNumber")
   
-  for(let i=0; i < counter; i++) {
+  for(let i=0; i < activeRound; i++) {
     roundNumber.textContent = rounds[i].round
   } 
 }
